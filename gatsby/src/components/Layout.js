9bedab51cefdb432a4004c6949999275c176ef12
Matt Doubet <mdoubet@gmail.com>
2020-09-23 16:17:55 -0600

    added logo, nav styles, and typogrophy using styled components

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -3,11 +3,13 @@ import Footer from './Footer';
 import Nav from './Nav';
 import GlobalStyles from '../styles/GlobalStyles';
 import 'normalize.css';
+import Typography from '../styles/Typography';
 
 export default function Layout({ children }) {
   return (
     <div>
       <GlobalStyles />
+      <Typography />
       <Nav />
       {children}
       <Footer />
