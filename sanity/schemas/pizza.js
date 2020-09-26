948aaea8e1b0e05b0952611fc0a909581fdb49a5
Matt Doubet <mdoubet@gmail.com>
2020-09-25 20:39:14 -0600

    customized sanity studio previews for pizzas to show a list of ingredients as subtitle

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,4 +1,5 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
+import toppings from './toppings';
 
 export default {
   // Computer name
@@ -39,5 +40,36 @@ export default {
       validation: (Rule) => Rule.min(600).max(4500),
       // TODO add custom component
     },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'toppings' }] }],
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+      topping4: 'toppings.4.name',
+      topping5: 'toppings.5.name',
+      topping6: 'toppings.6.name',
+      topping7: 'topping.7.name',
+      topping8: 'topping.8.name',
+      topping9: 'topping.9.name',
+    },
+    prepare: ({ title, media, ...toppingses }) => {
+      console.log(toppingses);
+
+      return {
+        title,
+        media,
+        subtitle: Object.values(toppingses).filter(Boolean).join(', '), // toppings object converted to array, filtered out empty ones
+      };
+    },
+  },
 };
