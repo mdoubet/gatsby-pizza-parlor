99a74755212d6bf6649a6aae2466c451b3f4f63d
Matt Doubet <mdoubet@gmail.com>
2020-09-30 15:22:55 -0600

    added person to schema

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -4,6 +4,7 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 import pizza from './pizza';
 import toppings from './toppings';
+import person from './person';
 // Then we give our schema to the builder and provide the result to Sanity
 
 export default createSchema({
@@ -11,5 +12,5 @@ export default createSchema({
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([pizza, toppings]),
+  types: schemaTypes.concat([pizza, toppings, person]),
 });
