1ea81c90f6e9ec91f1debb51f205548e920e319e
Matt Doubet <mdoubet@gmail.com>
2020-10-02 13:43:30 -0600

    Now displaying Pizzas on pizzas page with data from sanity incuding fluid image

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,43 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  const pizzas = data.allSanityPizza.nodes;
+  const { totalCount } = data.allSanityPizza;
+  console.log(totalCount);
+  console.log(pizzas);
   return (
     <>
-      <p>I'm the pizzas page</p>
+      <PizzaList pizzas={pizzas} pizzaCount={totalCount} />
     </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    allSanityPizza {
+      totalCount
+      nodes {
+        id
+        name
+        price
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+          vegetarian
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
